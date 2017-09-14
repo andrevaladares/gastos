48605d69e6e47a6c134703826fde85351a75283c
ur4z <smq0atpP>
2017-09-14 08:54:17 -0300

    preparando componente de gastos do mes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GastosMesComponent } from './gastos-mes/gastos-mes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosMesComponent
   ],
   imports: [
     BrowserModule,
